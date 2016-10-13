ced933bc1b177ce9dc3e5ce41ced8b4215b554c1
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-13 17:17:56 +0300

    Files and Directories - Lab

--- Files and Directories - Lab/04. Merge Files/Program.cs
+++ Files and Directories - Lab/04. Merge Files/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace _04.Merge_Files
     {
         static void Main(string[] args)
         {
+            string[] input1 = File.
+                ReadAllLines("FileOne.txt");
+            string[] input2 = File.
+                ReadAllLines("FileTwo.txt");
+            for (int i = 0; i < input1.Length; i++)
+            {
+                File.AppendAllText("output.txt",
+                    $"{input1[i]}{Environment.NewLine}");
+                File.AppendAllText("output.txt",
+                    $"{input2[i]}{Environment.NewLine}");
+            }
         }
     }
 }
