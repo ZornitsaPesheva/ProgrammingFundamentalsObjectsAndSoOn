b7e51f234057618461e235940a53656fad78f66e
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-13 16:31:53 +0300

    Files and Directories - Lab

--- Files and Directories - Lab/03. Word Count/Program.cs
+++ Files and Directories - Lab/03. Word Count/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace _03.Word_Count
 {
@@ -10,6 +12,35 @@ namespace _03.Word_Count
     {
         static void Main(string[] args)
         {
+            string[] words = File.ReadAllText("words.txt")
+                .ToLower().Split();
+            string[] text = File.ReadAllText("input.txt")
+                .ToLower().Split(new char[] 
+                {'\n', '\r', ' ', '.', ',', '!', '?', '-' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordCount =
+                new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                wordCount[word] = 0;
+            }
+
+            foreach (string word in text)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+            }
+
+            foreach (var word in wordCount.OrderByDescending(
+                p => p.Value))
+            {
+                  File.AppendAllText("output.txt", 
+                      $"{word.Key} - {word.Value}{Environment.NewLine}");
+            }
         }
     }
 }
