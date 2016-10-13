2f4d5c24b7251681792601f11e3e15b2267aab35
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-13 15:01:08 +0300

    Files and Directories - Lab

---
+++ Files and Directories - Lab/01. Odd Lines/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace _01.Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = File.ReadAllLines("input.txt");
+            //for (int i = 1; i < text.Length; i+=2)
+            //{
+            //    File.AppendAllText("output.txt", text[i] +
+            //        Environment.NewLine);
+            //}
+            File.WriteAllLines("output.txt",
+                text.Where((line, index) => index % 2 == 1));
+        }
+    }
+}
