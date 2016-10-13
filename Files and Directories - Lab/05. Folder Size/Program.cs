30bd33c9fd9d5f3fd7d78c5a02aa48aee45c2c55
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-13 17:50:05 +0300

    Files and Directories - Lab

--- Files and Directories - Lab/05. Folder Size/Program.cs
+++ Files and Directories - Lab/05. Folder Size/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace _05.Folder_Size
     {
         static void Main(string[] args)
         {
+            string[] files = Directory.GetFiles("TestFolder");
+            double sum = 0;
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+
+            File.WriteAllText("output.txt", sum.ToString());
         }
     }
 }
