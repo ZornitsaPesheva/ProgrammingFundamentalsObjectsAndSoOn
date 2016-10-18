ae947830349070b634fe02bcd05d4e7566279724
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-18 12:36:49 +0300

    Strings and RegEx - Lab

--- Strings and RegEx - Lab/02. Count Substring Occurrences/Program.cs
+++ Strings and RegEx - Lab/02. Count Substring Occurrences/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.Count_Substring_Occurrences
 {
@@ -10,6 +6,16 @@ namespace _02.Count_Substring_Occurrences
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine().ToLower();
+            string pattern = Console.ReadLine().ToLower();
+            int counter = 0;
+            int index = input.IndexOf(pattern);
+            while (index != -1)
+            {
+                counter++;
+                index = input.IndexOf(pattern, index + 1);
+            }
+            Console.WriteLine(counter);
         }
     }
 }
