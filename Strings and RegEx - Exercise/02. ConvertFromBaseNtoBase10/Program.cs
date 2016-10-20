bb556718b49b3ac6192abebd291945b9387c280a
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-20 06:52:18 +0300

    Strings and RegEx - Exercise

--- Strings and RegEx - Exercise/02. ConvertFromBaseNtoBase10/Program.cs
+++ Strings and RegEx - Exercise/02. ConvertFromBaseNtoBase10/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace _02.ConvertFromBaseNtoBase10
 {
@@ -10,6 +8,29 @@ namespace _02.ConvertFromBaseNtoBase10
     {
         static void Main(string[] args)
         {
+            BigInteger[] input = Console.ReadLine()
+                .Split().Select(BigInteger.Parse).ToArray();
+
+            BigInteger baseN = input[0];
+            BigInteger number = input[1];
+            BigInteger result = ConvertToBase10(number, baseN);
+            Console.WriteLine(result);
+        }
+
+        private static BigInteger ConvertToBase10(BigInteger n, BigInteger b)
+        {
+            BigInteger rem;
+            BigInteger sum = 0;
+            int p = 0;
+            while (n > 0)
+            {
+                rem = n % 10;
+                BigInteger bOnPower = BigInteger.Pow((BigInteger)b, p);
+                sum = sum + rem * bOnPower;
+                n = n / 10;
+                p++;
+            }
+            return sum;
         }
     }
 }
