f3c077a7e88263d6641885e8af092016bf884ebf
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-20 07:47:24 +0300

    Strings and RegEx - Exercise

--- Strings and RegEx - Exercise/04. Character Multiplier/Program.cs
+++ Strings and RegEx - Exercise/04. Character Multiplier/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace _04.Character_Multiplier
 {
@@ -10,6 +8,36 @@ namespace _04.Character_Multiplier
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine().Split().ToArray();
+            BigInteger sum = SumOfCharacters(input[0], input[1]);
+            Console.WriteLine(sum);
+        }
+
+        private static BigInteger SumOfCharacters(string v1, string v2)
+        {
+            BigInteger sum = new BigInteger();
+            int minLength = Math.Min(v1.Length, v2.Length);
+            for (int i = 0; i < minLength; i++)
+            {
+
+                int toAdd = v1[i] * v2[i];
+                sum += toAdd;
+            }
+            if (v1.Length > v2.Length)
+            {
+                for (int i = minLength; i < v1.Length; i++)
+                {
+                    sum += v1[i];
+                }
+            }
+            else if (v2.Length > v1.Length)
+            {
+                for (int i = minLength; i < v2.Length; i++)
+                {
+                    sum += v2[i];
+                }
+            }
+            return sum;
         }
     }
 }
