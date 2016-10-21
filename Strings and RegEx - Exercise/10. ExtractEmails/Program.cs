5a98d069601dfe0ac56bb7566bf23b3140343d15
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-21 11:20:39 +0300

    Strings and RegEx - Exercise

---
+++ Strings and RegEx - Exercise/10. ExtractEmails/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine()
+                .Split(new char[] {' '}
+                , StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string pattern 
+                = @"^([a-zA-Z]+[.]*)([a-zA-Z]*[-]*[a-zA-Z]*)[@][a-zA-Z]+[-]*[a-zA-Z]*[.][a-zA-Z]+([.]*[a-zA-Z])*$";
+
+            foreach (var item in text)
+            {
+
+            }
+
+        }
+    }
+}
